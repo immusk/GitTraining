756304110d930367aa17a527a968931ce6950e5e
shaiki <shaiki@192.168.0.3>
2015-08-15 14:53:08 +0530

    its my first commit

---
+++ Test1/src/Training/Testing1.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class Testing1 {
+
+	public Testing1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
